fe35cc85f985b03963503e5f88377a14bbdd849f
MuhammadLahin <muhammad.lahin@gmail.com>
2018-11-03 12:44:43 +0600

    Update forecast.js

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -3,7 +3,7 @@ import React from "react";
 const Forecast = props => {
   return (
     <div className="forecast">
-      <h2>{props.eachList.main.temp}</h2>
+      <h2>{props.eachList.main.temp}<span>&#x2103;</span></h2>
       <p>{props.eachList.dt_txt}</p>
     </div>
   );
