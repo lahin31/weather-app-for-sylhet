00a8419e370c2ad9ef023e2c9e56dfa33c570edd
MuhammadLahin <muhammad.lahin@gmail.com>
2018-11-05 05:12:19 +0600

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar = () => (
-  <nav className="Product_navbar">
+  <nav className="Syl-navbar">
     <h1>
       <i className="fas fa-home" /> Sylhet Weather App
     </h1>
