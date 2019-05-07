a742753bad5fff26bdbfb4c8ce0fcf8b9528e031
MuhammadLahin <muhammad.lahin@gmail.com>
2019-05-08 00:55:05 +0600

    Updates

---
+++ precache-manifest.66150afe85f53041af6709dac5f7971f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfb9ca3a106666034fdd",
+    "url": "/weather-app-sylhet/static/js/runtime~main.cfb9ca3a.js"
+  },
+  {
+    "revision": "ff3a0bb3f2fac547af21",
+    "url": "/weather-app-sylhet/static/js/main.ff3a0bb3.chunk.js"
+  },
+  {
+    "revision": "0bf31f4c2f1e8956b6c8",
+    "url": "/weather-app-sylhet/static/js/1.0bf31f4c.chunk.js"
+  },
+  {
+    "revision": "ff3a0bb3f2fac547af21",
+    "url": "/weather-app-sylhet/static/css/main.a0983775.chunk.css"
+  },
+  {
+    "revision": "b335b7ab1087dd6ddd7c7974ed923f75",
+    "url": "/weather-app-sylhet/index.html"
+  }
+];
